8a7598454e88d16d9d29695a382c8e0c097ff4c6
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2023-12-08 22:36:18 +0530

    feat:stake page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 // Import your components for different pages
 import HomePage from './pages/HomePage';
 import StakePage from './pages/StakePage';
-import AdminPage from './pages/AdminPage';
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/stake" element={<StakePage />} />
-        <Route exact path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/Button/index.js
@@ -0,0 +1,10 @@
+/**
+
+=========================================================
+** Button Exports - Styles can be edit on @stylesheet - styles/core/components/button
+=========================================================
+
+**/
+
+export * from "./Button";
+export * from "./ButtonGroup";
