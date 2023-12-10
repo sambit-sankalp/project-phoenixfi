b7858b55db8703ca1a8636a5d05e18bee55d1666
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-12-10 06:11:21 +0530

    image chanegs

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,17 @@
-import { SectionContainer } from '../components/commons/SectionContainer';
-import { PageTitle } from '../components/commons/PageTitle';
-import { Layout } from '../components/Layout/Layout';
-import { HomeBanner } from '../components/home/HomeBanner';
-import { ContentImage } from '../components/home/ContentImage';
-import { Content } from '../components/home/Content';
-import { MotionBTTContainer } from '../components/commons/MotionBTTContainer';
+import { SectionContainer } from "../components/commons/SectionContainer";
+import { PageTitle } from "../components/commons/PageTitle";
+import { Layout } from "../components/Layout/Layout";
+import { HomeBanner } from "../components/home/HomeBanner";
+import { ContentImage } from "../components/home/ContentImage";
+import { Content } from "../components/home/Content";
+import { MotionBTTContainer } from "../components/commons/MotionBTTContainer";
 import {
   CardBody,
   CardGroup,
   CardHeader,
   CardImage,
   Card,
-} from '../components/home/Card';
+} from "../components/home/Card";
 
 const HomePage = () => {
   return (
@@ -61,56 +61,6 @@ const HomePage = () => {
                   achieve your goals.
                 </p>
               </Content>
-              <CardGroup className="mt-10 flex w-[3/5] items-center justify-center">
-                <Card className="">
-                  <CardBody className="w-full bg-bgprimary p-12">
-                    <CardImage
-                      src="/features4.png"
-                      alt="Customizable Layouts image used."
-                    />
-                    <CardHeader className="mt-5 !text-[20px] !font-bold !text-white !opacity-100">
-                      Slashing Protection
-                    </CardHeader>
-                    <p className="!text-[16px] text-secondary-100 opacity-50">
-                      We require Storage Providers to cover potential slashing
-                      risks, ensuring your investments are protected, stable and
-                      secure.
-                    </p>
-                  </CardBody>
-                </Card>
-                <Card className="">
-                  <CardBody className="w-full bg-bgprimary p-12">
-                    <CardImage
-                      src="/features4.png"
-                      alt="Customizable Layouts image used."
-                    />
-                    <CardHeader className="mt-5 !text-[20px] !font-bold !text-white !opacity-100">
-                      Maximized Profitability
-                    </CardHeader>
-                    <p className="!text-[16px] text-secondary-100 opacity-50">
-                      CPartnering with Collectif DAO allows Storage Providers to
-                      earn up to 40% more in mining fees, maximizing rewards for
-                      stakers.
-                    </p>
-                  </CardBody>
-                </Card>
-                <Card className="">
-                  <CardBody className="w-full bg-bgprimary p-12">
-                    <CardImage
-                      src="/features4.png"
-                      alt="Customizable Layouts image used."
-                    />
-                    <CardHeader className="mt-5 !text-[20px] !font-bold !text-white !opacity-100">
-                      Optimal Distribution
-                    </CardHeader>
-                    <p className="!text-[16px] text-secondary-100 opacity-50">
-                      Your capital is allocated to top-tier, geographically
-                      distributed Storage Providers in the Filecoin network for
-                      enhanced efficiency.
-                    </p>
-                  </CardBody>
-                </Card>
-              </CardGroup>
             </SectionContainer>
           </MotionBTTContainer>
         </SectionContainer>
